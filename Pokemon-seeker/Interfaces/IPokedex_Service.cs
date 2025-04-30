35f7e3bee5101516b6ffc1d5e11e2e3f1b150b81
= <=>
2025-04-30 20:37:12 -0300

    fix: Rename and remove some classes for best structure

--- Pokemon-seeker/Interfaces/IPokedex_Service.cs
+++
@@ -1,9 +0,0 @@
-using System;
-using Pokemon_seeker.Models;
-
-namespace Pokemon_seeker.Interfaces;
-
-public interface IPokedex_Service
-{
-    public Task<ResponseModel> GetPokemonAsync(string slug);
-}
