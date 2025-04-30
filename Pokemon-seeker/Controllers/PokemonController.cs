c66049f7827da631d7e12403a09a84a280798d34
= <=>
2025-04-30 20:52:49 -0300

    fix: To adapt the pokemonController to new structure by returning a viewModel and Dto

--- Pokemon-seeker/Controllers/PokemonController.cs
+++ Pokemon-seeker/Controllers/PokemonController.cs
@@ -1,9 +1,12 @@
 using System.Net;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-using Pokemon_seeker.Interfaces;
-using Pokemon_seeker.Models;
-using Pokemon_Seeker.Models;
+using Newtonsoft.Json;
+using Pokemon_seeker.DTOs.Responses;
+using Pokemon_seeker.Mappers;
+using Pokemon_seeker.Presentation.ViewModels;
+using Pokemon_seeker.Services.Implementations;
+using Pokemon_seeker.Services.Interfaces;
 
 
 namespace Pokemon_seeker.Controllers
@@ -13,18 +16,39 @@ namespace Pokemon_seeker.Controllers
     public class PokemonController : ControllerBase
     {
         private readonly IPokedex_Service _pokedex_Service;
-        public PokemonController([FromServices]IPokedex_Service pokedex_Service)
+        public PokemonController(
+            [FromServices]IPokemonJsonConverter pokemonJsonConverter,
+            IPokedex_Service pokedex_Service
+        )
         {
             _pokedex_Service = pokedex_Service;
         }
         [HttpGet("{slug}")]
-        [ProducesResponseType(typeof(ResponseModel),StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(BaseResponseModel),StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
         [ProducesResponseType(StatusCodes.Status500InternalServerError)]
-        public async Task<ActionResult<ResponseModel>> GetPokemon([FromRoute] string slug)
+        public async Task<ActionResult<BaseResponseModel>> GetPokemon([FromRoute] string slug)
         {
-            var pokemonResult = await _pokedex_Service.GetPokemonAsync(slug);
-            return StatusCode((int)pokemonResult.HttpStatusCode,pokemonResult);
+            PokemonViewModel pokemonView;
+
+            //Get response from request
+            HttpResponseMessage responseMessage = await _pokedex_Service.GetPokemonAsync(slug);
+            //get status code
+            var statusCode = responseMessage.StatusCode;
+            // Get success result
+            var succeeded = responseMessage.IsSuccessStatusCode;
+            //Build error pokemon_view_model
+            if(!succeeded)
+                pokemonView = new PokemonViewModel(succeeded,statusCode);
+            else
+            {
+                //Serialize response_message content
+                var pokemonObject = await new PokemonJsonConverter().ToObject(responseMessage);
+                //Convert the Pokemon object to DTO
+                var pokemonDtodto = new PokemonDto().ToDto(pokemonObject);
+                pokemonView = new PokemonViewModel(succeeded,statusCode,pokemonDtodto);
+            }
+            return StatusCode((int)statusCode,pokemonView);
         }
     }
 }
