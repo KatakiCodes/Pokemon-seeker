7cfad35186c25ccf07eacc68978949b9ef276fa9
= <=>
2025-06-30 20:34:37 +0100

    fix: Usando record nas DTOs

--- Pokemon-seeker/DTOs/Responses/PokemonDto.cs
+++ Pokemon-seeker/DTOs/Responses/PokemonDto.cs
@@ -2,7 +2,7 @@ using System;
 
 namespace Pokemon_seeker.DTOs.Responses;
 
-public class PokemonDto
+public record PokemonDto
 {
     public int Id { get; set; }
     public string Name { get; set; }
