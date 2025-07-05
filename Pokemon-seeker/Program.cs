5c0052ec5215977fa7e4a68f36af8f7840eca7ad
Nelson Dos Santos <katakicode001@gmail.com>
2025-07-05 22:24:26 +0100

    fix: Configurando tudo para o primeiro deploy

--- Pokemon-seeker/Program.cs
+++ Pokemon-seeker/Program.cs
@@ -6,22 +6,23 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddControllers();
-builder.Services.AddOpenApi();
 builder.Services.AddSwaggerGen();
 
 builder.Services.AddScoped<IPokedex_Service, Pokedex_Service>();
 builder.Services.AddScoped<IPokemonJsonConverter, PokemonJsonConverter>();
-
+builder.Services.AddCors(option =>
+{
+    option.AddPolicy("AllowAll", policy =>
+    {
+        policy.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod();
+    });
+});
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.MapOpenApi();
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
+app.MapOpenApi();
+app.UseSwagger();
+app.UseSwaggerUI();
+app.UseCors("AllowAll");
 app.MapControllers();
 app.UseHttpsRedirection();
 app.Run();
